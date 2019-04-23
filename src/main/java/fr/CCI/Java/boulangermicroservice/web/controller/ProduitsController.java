93d5328fbaea8fd64b3db23148be44b2bf03be6d
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-23 16:57:57 +0200

    get produitParID et post newProduit

--- src/main/java/fr/CCI/Java/boulangermicroservice/web/controller/ProduitsController.java
+++ src/main/java/fr/CCI/Java/boulangermicroservice/web/controller/ProduitsController.java
@@ -2,9 +2,15 @@ package fr.CCI.Java.boulangermicroservice.web.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.CCI.Java.boulangermicroservice.dao.ProduitDAO;
@@ -18,7 +24,31 @@ public class ProduitsController {
 	@GetMapping(value= "/produits")
 	public List<Produit> listeDesProduits(){
 		return produitDao.getProduits();
-		
 	}
+	
+	@GetMapping(value= "/produits/{id}")
+	public Produit produitParID(@PathVariable int id) {
+		return produitDao.getByID(id);
+	}
+	
+	
+	@PostMapping(value = "/produits")
+    public void ajouterProduit(@RequestBody Produit produit){
+		produitDao.addProduit(produit);
+	}
+	
+	
+	
+	
+	
+	/*
+    @PostMapping (value = "/produits")
+    public void ajouterProduit(@RequestBody Produit produit){
+    	produitDao.addProduit(produit);
+
+        //return new ResponseEntity<Commande>(commande, HttpStatus.CREATED);
+    }
+    
+    */
 
 }
