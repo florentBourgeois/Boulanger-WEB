75b3a34a2cfe9e92fc83b4552b28b62fec6da649
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-24 10:00:38 +0200

    ajout de @entity dans produit

--- src/main/java/fr/CCI/Java/boulangermicroservice/model/Produit.java
+++ src/main/java/fr/CCI/Java/boulangermicroservice/model/Produit.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+@Entity
 public class Produit {
 	
 	@Id
