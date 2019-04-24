55b6d032fd69609b76e5b4c5db754692b12190e8
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-24 11:47:09 +0200

    ajout de nouveaux produits dans le fichier sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,15 +4,25 @@ INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG )
 
 
 INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
-		VALUES (1, 'Baguette', 1, 'http://url.jpg');
+		VALUES (1, 'Baguette', 1, 'https://staticmedia.fauchon.com/media/catalog/product/cache/2/image/480x/040ec09b1e35df139433887a97daa66f/1/0/1008690.jpg');
 
 
 
 INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
-		VALUES (2, 'Tarte', 10, 'https://www.biofournil.com/wp-content/uploads/2015/07/pain-rustique-rt-800x510.png');
+		VALUES (2, 'Tarte', 10, 'https://static.cuisineaz.com/400x320/i113644-tarte-normande-pommes.jpg');
 
 
 
 
 INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
-		VALUES (3, 'Gateau', 20, 'https://www.biofournil.com/wp-content/uploads/2015/07/pain-rustique-rt-800x510.png');
+		VALUES (3, 'Gateau', 20, 'https://static.cuisineaz.com/400x320/i91729-gateau-simple-au-chocolat.jpg');
+
+
+
+
+INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
+		VALUES (4, 'Arc-en-Ciel', 200, 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/DesktopImages/Rainbow-Cake600x600_2.jpg?ext=.jpg);
+
+
+INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
+		VALUES (5, 'Baba', 50, 'https://natashaskitchen.com/wp-content/uploads/2012/06/cupcake-recipe-1-500x467.jpg);
