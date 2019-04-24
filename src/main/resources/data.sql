7e19f264b300ec064259d91fdd2e11f9f03a6108
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-24 10:29:12 +0200

    ajout du fichier data.sql qui rempli la base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
+		VALUES (0, 'Pain', 2, 'https://www.biofournil.com/wp-content/uploads/2015/07/pain-rustique-rt-800x510.png');
+
+
+
+INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
+		VALUES (1, 'Baguette', 1, 'http://url.jpg');
+
+
+
+INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
+		VALUES (2, 'Tarte', 10, 'https://www.biofournil.com/wp-content/uploads/2015/07/pain-rustique-rt-800x510.png');
+
+
+
+
+INSERT INTO PRODUIT (ID , NOM ,VALEUR ,IMG ) 
+		VALUES (3, 'Gateau', 20, 'https://www.biofournil.com/wp-content/uploads/2015/07/pain-rustique-rt-800x510.png');
