87e1310aceaaa42ed9102fde1b2bb9c19eb855e6
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-24 09:48:19 +0200

    ajout de h2 et du produit entity

---
+++ src/main/java/fr/CCI/Java/boulangermicroservice/model/Produit.java
@@ -0,0 +1,73 @@
+package fr.CCI.Java.boulangermicroservice.model;
+
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Produit {
+	
+	@Id
+	@GeneratedValue
+	private int id; 
+	private String nom;
+	private int valeur;
+	private String img;
+	
+	
+	public Produit() {
+	}
+	
+	
+	public Produit(int id,String nom, int valeur, String img) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.valeur = valeur;
+		this.img = img;
+	}
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getValeur() {
+		return valeur;
+	}
+	public void setValeur(int valeur) {
+		this.valeur = valeur;
+	}
+
+	
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	@Override
+	public String toString() {
+		return id+ " " + nom + " vendu à " + valeur + " euro";
+	}
+	
+	
+
+	
+}
